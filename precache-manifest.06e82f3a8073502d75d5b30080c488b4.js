fbbf8facde05ce081f703f687f5aa2a614ea594d
MikhailGorbunov <68015812+MikhailGorbunov@users.noreply.github.com>
2022-12-09 15:11:17 +0000

    Updates

---
+++ precache-manifest.06e82f3a8073502d75d5b30080c488b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5de85c1b073acafb0a6c",
+    "url": "/mig_portfolio/static/css/main.13aa51f9.chunk.css"
+  },
+  {
+    "revision": "5de85c1b073acafb0a6c",
+    "url": "/mig_portfolio/static/js/main.5de85c1b.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/mig_portfolio/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "fe9e49dcc9c458a203f8",
+    "url": "/mig_portfolio/static/js/2.fe9e49dc.chunk.js"
+  },
+  {
+    "revision": "8edf89abc37d280b8954",
+    "url": "/mig_portfolio/static/js/runtime~main.8edf89ab.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/mig_portfolio/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "adc3ac36f49cc16bd9769cdbe799fbed",
+    "url": "/mig_portfolio/index.html"
+  }
+];
